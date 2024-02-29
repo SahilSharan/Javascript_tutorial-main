394fe432f6492e94743564cb4f6d95b832eb6c59
SahilSharan <sahilthakur73153@gmail.com>
2024-03-01 01:08:24 +0530

    new changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,22 @@ let negValue = -value
 
 // console.log(negValue);
 
+let str1= "Hello"
+let str2= " Sahil"
+
+let str3 = str1 + str2 
+
+// console.log(srt3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1 + 2");
+// console.log(1 +2 + "2");
+
+console.log(+true) /*gives 1 as output */
+console.log(+ ""); /* gives 0 as output */
+
+let gameCounter = 100 
+gameCounter++
+
+console.log(gameCounter);
